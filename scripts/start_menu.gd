e7871b03791f1e4b40e1906ed0742db8b633c97f
erickmrlsn-ui <erick.mrlsn@gmail.com>
2026-05-17 11:11:13 -0600

    Add start menu and audio setup

---
+++ scripts/start_menu.gd
@@ -0,0 +1,18 @@
+extends Control
+
+@onready var play_button: Button = $PlayButton
+@onready var fade_rect: ColorRect = $FadeRect
+
+func _ready() -> void:
+	fade_rect.color = Color(0, 0, 0, 0)
+	play_button.pressed.connect(_on_play_pressed)
+
+func _on_play_pressed() -> void:
+	play_button.disabled = true
+
+	var tween := create_tween()
+	tween.tween_property(fade_rect, "color", Color(0, 0, 0, 1), 1.0)
+	tween.finished.connect(_go_to_story)
+
+func _go_to_story() -> void:
+	get_tree().change_scene_to_file("res://scenes/StoryIntro.tscn")
